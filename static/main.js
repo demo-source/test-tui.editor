24af227ae18c58646732944ab3f3725c5b0c441d
IgorNovozhilov <maildrakon@gmail.com>
2020-07-24 00:41:59 +0300

    test Grid

--- static/main.js
+++ static/main.js
@@ -1,4 +1,30 @@
 const { Editor } = window.toastui
+const { Grid } = window.tui
+const grid = new Grid({
+  el: document.getElementById('grid'),
+  data: [{
+    name: 'name',
+    title: 'title'
+  }],
+  scrollX: false,
+  scrollY: false,
+  columns: [
+    {
+      header: 'Page name',
+      name: 'name',
+      editor: 'text'
+    },
+    {
+      header: 'Page title',
+      name: 'title',
+      editor: 'text'
+    }
+  ]
+})
 const editor = new Editor({
-  el: document.querySelector('#editor')
+  el: document.querySelector('#editor'),
+  height: 'auto',
+  previewStyle: 'vertical',
+  initialEditType: 'wysiwyg',
+  language: 'ru'
 })
